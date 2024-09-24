90f1a0fc7b1640055c986795eb90ce41bd5322b7
Melezwa26 <phalosenzwa@gmail.com>
2024-09-24 07:12:06 +0200

    update frontend module

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    getNames();
+
+  }, [])
+
+  const getNames = async () => {
+    const response = await axios.get('/names');
+    console.log(response);
+    setUsername(response.data)
+  }
+   return (
+    <h1>My frontend</h1>,
+    <h3>My Jo jo {Joing}</h3>
+
+    
   );
 }
 
